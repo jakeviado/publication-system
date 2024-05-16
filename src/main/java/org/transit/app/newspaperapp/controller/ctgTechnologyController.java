ac70958b19a4051d84753cbd7dee71590ce1d219
elaguilarr <aguilar.elashaina13@gmail.com>
2024-05-16 22:37:56 +0800

    commit Ela

---
+++ src/main/java/org/transit/app/newspaperapp/controller/ctgTechnologyController.java
@@ -0,0 +1,20 @@
+package org.transit.app.newspaperapp.controller;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.VBox;
+import org.transit.app.newspaperapp.services.ArticleTr;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ctgTechnologyController extends articleLoader implements Initializable {
+    @FXML
+    public VBox ctgryTechnologyContainer;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        ArticleTr load  = new ArticleTr();
+        loadTechnologyArticles(load, ctgryTechnologyContainer);
+    }
+}
