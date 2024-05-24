902ffcb484a0520ecd29b88c82779c221dfb135e
elaguilarr <aguilar.elashaina13@gmail.com>
2024-05-25 04:00:58 +0800

    commit Ela

--- src/main/java/org/transit/app/newspaperapp/controller/mainpage/Account/accountController.java
+++ src/main/java/org/transit/app/newspaperapp/controller/mainpage/Account/accountController.java
@@ -2,9 +2,27 @@ package org.transit.app.newspaperapp.controller.mainpage.Account;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.event.ActionEvent;
 
 public class accountController {
 
     @FXML
-    public Button changePassBtn;
+    public Button savedArticlesBtn;
+
+    @FXML
+    public Button accountSettingsBtn;
+
+    @FXML
+    public void initialize() {
+        savedArticlesBtn.setOnAction(this::savedArticlesBtnAction);
+        accountSettingsBtn.setOnAction(this::accountSettingsBtnAction);
+    }
+
+    public void savedArticlesBtnAction(ActionEvent event) {
+        System.out.println("Articles Saved!");
+    }
+
+    public void accountSettingsBtnAction(ActionEvent event) {
+        System.out.println("Account Settings");
+    }
 }
