e9f210669a71b5f5a9bc02ca2c06743776125e9f
elaguilarr <aguilar.elashaina13@gmail.com>
2024-05-25 05:03:13 +0800

    commit Ela

--- src/main/java/org/transit/app/newspaperapp/controller/mainpage/Account/accountController.java
+++ src/main/java/org/transit/app/newspaperapp/controller/mainpage/Account/accountController.java
@@ -1,8 +1,14 @@
 package org.transit.app.newspaperapp.controller.mainpage.Account;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
 import javafx.event.ActionEvent;
+import javafx.scene.Node;
+import java.io.IOException;
 
 public class accountController {
 
@@ -19,10 +25,34 @@ public class accountController {
     }
 
     public void savedArticlesBtnAction(ActionEvent event) {
-        System.out.println("Articles Saved!");
+        loadSavedArticlesScene(event);
     }
 
     public void accountSettingsBtnAction(ActionEvent event) {
-        System.out.println("Account Settings");
+        loadAccountSettingsScene(event);
+    }
+
+    private void loadSavedArticlesScene(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("savedArticle.fxml"));
+            Parent root = loader.load();
+            Scene scene = new Scene(root);
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow(); // Get the current stage
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            System.out.println("Articles Saved!");
+        }
+    }
+
+    private void loadAccountSettingsScene(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("accountSettings.fxml"));
+            Parent root = loader.load();
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.setScene(new Scene(root));
+        } catch (IOException e) {
+            System.out.println("Account Settings");
+        }
     }
 }
